c522fe14b1148afd0d2c45e1faeea69cc8e163b4
Mayur Barge <mayurbarge226@gmail.com>
2025-05-15 09:07:47 +0530

    Adding Trapezoid validations to check it has only single parallel edge pair and four edges

--- src/main/scala/Trapezoid.scala
+++ src/main/scala/Trapezoid.scala
@@ -1,1 +1,11 @@
+import zio.prelude.Validation
+
 case class Trapezoid(override val edges: List[Line]) extends Quadrilateral(edges)
+
+object Trapezoid {
+  def validateTrapezoid(trapezoid: Trapezoid) = {
+    if(trapezoid.parallelEdgesPair.size == 1 && trapezoid.edges.size == 4)
+      Validation.succeed(trapezoid)
+    else Validation.fail("Invalid Trapezoid.")
+  }
+}
--- src/test/scala/TrapezoidSpec.scala
+++ src/test/scala/TrapezoidSpec.scala
@@ -35,5 +35,43 @@ object TrapezoidSpec extends ZIOSpecDefault {
       assertTrue(trapezoid.parallelEdgesPair.size == 1)
     },
 
+    test("Trapezoid validation should fail when it does not have four edges") {
+      val lineAB = Line(Point(0,0), Point(4, 0))
+      val lineBC = Line(Point(4,0), Point(2,1))
+      val lineCD = Line(Point(2,1), Point(1,1))
+
+      val trapezoid = Trapezoid(NonEmptyList(lineAB, lineBC, lineCD))
+      assert(Trapezoid.validateTrapezoid(trapezoid))(equalTo(Validation.fail("Invalid Trapezoid.")))
+    },
+
+    test("Trapezoid validation should fail when it does not have any parallel edge pair") {
+      val lineAB = Line(Point(0,0), Point(4, 0))
+      val lineBC = Line(Point(4,0), Point(3,1))
+      val lineCD = Line(Point(3,1), Point(1,2))
+      val lineDA = Line(Point(1,2), Point(0,0))
+
+      val trapezoid = Trapezoid(NonEmptyList(lineAB, lineBC, lineCD, lineDA))
+      assert(Trapezoid.validateTrapezoid(trapezoid))(equalTo(Validation.fail("Invalid Trapezoid.")))
+    },
+
+    test("Trapezoid validation should fail when it does two parallel edge pairs") {
+      val lineAB = Line(Point(0,0), Point(1, 0))
+      val lineBC = Line(Point(1,0), Point(1,1))
+      val lineCD = Line(Point(1,1), Point(0,1))
+      val lineDA = Line(Point(0,1), Point(0,0))
+
+      val trapezoid = Trapezoid(NonEmptyList(lineAB, lineBC, lineCD, lineDA))
+      assert(Trapezoid.validateTrapezoid(trapezoid))(equalTo(Validation.fail("Invalid Trapezoid.")))
+    },
+
+    test("Trapezoid validation should pass when it has exactly one parallel edge pair") {
+      val lineAB = Line(Point(0,0), Point(4, 0))
+      val lineBC = Line(Point(4,0), Point(3,1))
+      val lineCD = Line(Point(3,1), Point(2,1))
+      val lineDA = Line(Point(2,1), Point(0,0))
+
+      val trapezoid = Trapezoid(NonEmptyList(lineAB, lineBC, lineCD, lineDA))
+      assert(Trapezoid.validateTrapezoid(trapezoid))(equalTo(Validation.succeed(trapezoid)))
+    },
   )
 }
