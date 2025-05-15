e7cd63dba4e90e8de5758a2a09938798caa04fcd
Mayur Barge <mayurbarge226@gmail.com>
2025-05-15 07:00:38 +0530

    Adding validation checks for Square

--- src/main/scala/Square.scala
+++ src/main/scala/Square.scala
@@ -1,5 +1,14 @@
+import zio.prelude.Validation
+
 case class Square(override val edges: List[Line]) extends Quadrilateral(edges) {
-  def parallelEdges = edges.combinations(2).filter(pairOfEdges =>
+  def parallelEdgesPair = edges.combinations(2).filter(pairOfEdges =>
     Line.isParallel(pairOfEdges.head, pairOfEdges.last)
   ).toList
+}
+object Square {
+  def validateSquare(square: Square) = {
+    if(square.parallelEdgesPair.size  == 2 && square.thetaOne.contains(90))
+      Validation.succeed(square)
+    else Validation.fail("Invalid square.")
+  }
 }
--- src/test/scala/SquareSpec.scala
+++ src/test/scala/SquareSpec.scala
@@ -32,9 +32,53 @@ object SquareSpec extends ZIOSpecDefault {
       val lineDA = Line(Point(0,1), Point(0,0))
       val square = Square(List(lineAB, lineBC, lineCD, lineDA))
 
-      assertTrue(square.parallelEdges.size == 2)
-      assert(square.parallelEdges.head)(hasSameElements(List(lineAB, lineCD)))
-      assert(square.parallelEdges.last)(hasSameElements(List(lineBC, lineDA)))
+      assertTrue(square.parallelEdgesPair.size == 2)
+      assert(square.parallelEdgesPair.head)(hasSameElements(List(lineAB, lineCD)))
+      assert(square.parallelEdgesPair.last)(hasSameElements(List(lineBC, lineDA)))
+    },
+
+    test("Square should an 90 degree angle between intersecting edges") {
+      val lineAB = Line(Point(0,0), Point(1, 0))
+      val lineBC = Line(Point(1,0), Point(1,1))
+      val lineCD = Line(Point(1,1), Point(0,1))
+      val lineDA = Line(Point(0,1), Point(0,0))
+      val square = Square(List(lineAB, lineBC, lineCD, lineDA))
+
+      val angleBetweenEdges = square.thetaOne
+      assertTrue(angleBetweenEdges.contains(90))
+    },
+
+    test("Validation should pass when Square has two parallel edges pair and theta 90 degree") {
+      val lineAB = Line(Point(0,0), Point(1, 0))
+      val lineBC = Line(Point(1,0), Point(1,1))
+      val lineCD = Line(Point(1,1), Point(0,1))
+      val lineDA = Line(Point(0,1), Point(0,0))
+      val square = Square(List(lineAB, lineBC, lineCD, lineDA))
+      val validatedSquare = Square.validateSquare(square)
+
+      assert(validatedSquare)(equalTo(Validation.succeed(square)))
+    },
+
+    test("Validation should fail if edges are not parallel") {
+      val lineAB = Line(Point(0,0), Point(1, 0))
+      val lineBC = Line(Point(1,0), Point(2,2))
+      val lineCD = Line(Point(2,2), Point(0,1))
+      val lineDA = Line(Point(0,1), Point(0,0))
+      val square = Square(List(lineAB, lineBC, lineCD, lineDA))
+      val validatedSquare = Square.validateSquare(square)
+
+      assert(validatedSquare)(equalTo(Validation.fail("Invalid square.")))
+    },
+
+    test("Validation should fail if theta is not 90 degree") {
+      val lineAB = Line(Point(0,0), Point(1, 0))
+      val lineBC = Line(Point(1,0), Point(2,1))
+      val lineCD = Line(Point(2,1), Point(1,1))
+      val lineDA = Line(Point(1,1), Point(0,0))
+      val square = Square(List(lineAB, lineBC, lineCD, lineDA))
+      val validatedSquare = Square.validateSquare(square)
+
+      assert(validatedSquare)(equalTo(Validation.fail("Invalid square.")))
     },
 
   )
